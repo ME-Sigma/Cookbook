0bbc5687ea69df77634c75bae6fe8584ed3668b7
ME-Sigma <roncoben@web.de>
2019-07-17 19:16:44 +0200

    Adding user_input and variable_types programs

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# print("Hello.")
+# name: str = input("What is your name? ")
+# print("Hello " + name)
+
+number = input("What is your favorite number? ")
+print("This Number multiplied by 3 is: " + str(int(number) * 3))
---
+++ variables_types.py
@@ -0,0 +1,10 @@
+my_interger = 3;
+my_froat = 3.14592
+my_string = "hello"
+
+print(type(my_interger))
+print(type(my_froat))
+print(type(my_string))
+
+my_interger = "some text"
+print(type(my_interger))
